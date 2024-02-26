8dc9011c5ecdbb9df2571fed6aebdf073cb1e0a0
Rakibul Islam <rakibulrakib05@gmail.com>
2024-02-27 02:39:05 +0600

    create react router project and loaded data

---
+++ src/Component/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h2>OrderReview hare...</h2>
+        </div>
+    );
+};
+
+export default OrderReview;
